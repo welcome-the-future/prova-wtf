25d7f27e406e5c33d779f537d57397957967e9b4
Rafael Marques <rafaelmarquespaixao@outlook.com>
2019-09-15 00:46:35 -0300

    Diagrama de banco

--- DB/PROVA_WTF.sql
+++ DB/PROVA_WTF.sql
@@ -1,17 +1,26 @@
+--CRIA O BANCO DE DADOS
+CREATE DATABASE PROVA_WTF
+
+-- CRIA AS TABELAS
 CREATE TABLE usuarios(
-	id_usuario bigint not null primary key,
-	nome_completo varchar(250) not null
+	id_usuario bigint not null primary key identity,
+	nome_completo varchar(250) not null,
+	departamento varchar(200) not null,
+	cargo varchar(200) not null,
+	deletado bit
 )
 
 CREATE TABLE salas(
-	   id_sala bigint primary key,
+	   id_sala bigint primary key identity,
 	   nome_sala  varchar(200) not null,
-	   localizacao_sala varchar(200) not null
+	   localizacao_sala varchar(200) not null,
+	   deletado bit
 )
 
 CREATE TABLE recursos_sala(
-	  id_recurso bigint primary key,
-	  descricao varchar(200) not null
+	  id_recurso bigint primary key identity,
+	  descricao varchar(200) not null,
+	  deletado bit
 )
 
 CREATE TABLE rl_salas_recursos(
@@ -23,11 +32,17 @@ CREATE TABLE rl_salas_recursos(
 	 foreign key (id_recurso) references recursos_sala(id_recurso)
 )
 
+CREATE TABLE agendamento_status(id_status bigint primary key, descricao varchar(200))
+
 CREATE TABLE agendamento(
-	 id_agendamento bigint primary key,
-	 data_hora datetime not null,
+	 id_agendamento bigint primary key identity,
+	 data_agendamento date not null,
+	 hora_inicio time not null,
+	 hora_fim time not null,
 	 id_sala bigint not null,
-	 foreign key (id_sala) references salas(id_sala)
+	 id_status bigint not null
+	 foreign key (id_sala) references salas(id_sala),
+	 foreign key (id_status) references agendamento_status(id_status)
 )
 
 CREATE TABLE agendamento_usuarios(
